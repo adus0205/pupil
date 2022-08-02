a711247cf5adf528393872b8469d9eda5ae54a87
adisz <adiszu89@gmail.com>
2022-08-02 17:30:43 +0200

    Initial commit

---
+++ src/main/java/com/szupke/pupil/PupilApplication.java
@@ -0,0 +1,13 @@
+package com.szupke.pupil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PupilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PupilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szupke/pupil/PupilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szupke.pupil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PupilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
